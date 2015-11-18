07ea263d66844d0c987dac24c4984c7ba397bda8
Chun Zujun <chunzj@huoli.com>
2015-11-18 09:31:03 +0800

    create server

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+var http = require('http');
+http.createServer(function (req,res) {
+  console.log('server is added!');//发了两次请求，另外一次请求icon
+  res.end('hello world!');
+}).listen(8081,function(){
+  console.log('listen is added!');//建立了链接之后立马输出
+});
