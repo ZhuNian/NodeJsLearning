ebe05163b0f70efdd19705856b8ca9b2b91d2983
ZhuNian <zhunianhhhxy@163.com>
2015-11-25 15:41:51 +0800

    git shortcut try

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ http.createServer(function (req,res) {
   console.log('server is added!');//发了两次请求，另外一次请求icon
   res.end('hello world!');
 }).listen(8081,function(){
-  console.log('listen is added!');//建立了链接之后立马输出
+  console.log('listen is added!');//建立了连接之后立马输出
 });
 console.log('Server running at http://127.0.0.1:8081/');
